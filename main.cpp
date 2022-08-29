314c9693856728597a7755ed4ed6ede201f7ce4a
SilentUniverse <2755565704@qq.com>
2022-08-30 00:06:38 +0800

    实现 sort

--- main.cpp
+++ main.cpp
@@ -3,7 +3,16 @@
 using namespace std;
 
 void sort(int arr[], int size) {
-
+    int tmp = 0;
+    for (size_t i = 0; i < size -1; i++) {
+        for (size_t j = 0; j < size - 1 -i; j++) {
+            if (arr[j] > arr[j + 1]) {
+                tmp = arr[j];
+                arr[j] = arr[j + 1];
+                arr[j + 1] = tmp;
+            }
+        }
+    }
 }
 
 
