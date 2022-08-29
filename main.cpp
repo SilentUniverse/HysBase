ba6f28076fcfc7c71150ffe4470c2b9e4af36e65
hys <hys@cc.local>
2022-08-29 23:57:07 +0800

    创建 main.cpp

---
+++ main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+void sort(int arr[], int size) {
+
+}
+
+
+int main() {
+    int arr[] = {13, 22, 32, 4, 35, 61, 73, 18};
+    int size = sizeof(arr)/sizeof(arr[0]);
+    sort(arr, size);
+    return 0;
+}
+
